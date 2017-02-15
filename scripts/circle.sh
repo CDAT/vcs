84e535ff2c37ab7476751e6a096cd4255549f4de
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-14 20:30:42 -0800

    missing dependencies for testing

--- scripts/circle.sh
+++ scripts/circle.sh
@@ -4,7 +4,7 @@ USER=uvcdat
 export PATH=${HOME}/miniconda2/bin:${PATH}
 echo "Trying to upload conda"
 conda update -y -q conda
-conda install -c conda-forge -c uvcdat uvcdat-nox pyopenssl
+conda install -c conda-forge -c uvcdat uvcdat-nox pyopenssl nose image-compare
 export UVCDAT_ANONYMOUS_LOG=False
 vcs_download_sample_data
 conda config --set anaconda_upload no
