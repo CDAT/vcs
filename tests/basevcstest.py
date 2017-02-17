4a0878ff27f282e46bfebd46a67bfc77179ca952
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-17 14:07:03 -0800

    chugging along

--- tests/basevcstest.py
+++ tests/basevcstest.py
@@ -53,5 +53,28 @@ class VCSBaseTest(unittest.TestCase):
         self.assertEqual(ret,0)
         return ret
 
+    def check_values_setting(self,gm,attributes,good_values=[],bad_values=[]):
+      if isinstance(attributes,str):
+	attributes=[attributes,]
+      for att in attributes:
+	for val in good_values:
+	  setattr(gm,att,val)
+	for val in bad_values:
+	  try:
+	    setattr(gm,att,val)
+	    success = True
+	  except:
+	    success = False
+	  else:
+	    if success:
+	      if hasattr(gm,"g_name"):
+		nm = gm.g_name
+	      elif hasattr(gm,"s_name"):
+		nm = gm.s_name
+	      else:
+		nm=gm.p_name
+	      raise Exception,"Should not be able to set %s attribute '%s' to %s" % (nm,att,repr(val))
+
+
 def run():
     unittest.main()
