24b7787ad3d3adb192f99dea4c74d56e8725aeb6
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-17 17:13:50 -0800

    correct script

--- scripts/circleci_mac_machine_pre.sh
+++ scripts/circleci_mac_machine_pre.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-export PATH=${HOME}/miniconda2/bin:${PATH}
-echo "Trying to upload conda"
+curl https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -o miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH=${HOME}/miniconda/bin:${PATH}
+conda config --set always_yes yes --set changeps1 no
 conda update -y -q conda
-conda install -c conda-forge -c uvcdat uvcdat-nox pyopenssl nose image-compare
+conda install -c conda-forge -c uvcdat uvcdat pyopenssl nose image-compare
 vcs_download_sample_data
 conda config --set anaconda_upload no
