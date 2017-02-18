5fcfa25a75906d40cc8a9b36a34846b2c72188df
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-17 17:47:15 -0800

    another round

--- scripts/circleci_mac_machine_pre.sh
+++ scripts/circleci_mac_machine_pre.sh
@@ -4,7 +4,4 @@ bash miniconda.sh -b -p $HOME/miniconda
 export PATH=${HOME}/miniconda/bin:${PATH}
 conda config --set always_yes yes --set changeps1 no
 conda update -y -q conda
-conda install -c conda-forge -c uvcdat uvcdat pyopenssl nose image-compare
-export UVCDAT_ANONYMOUS_LOG=False
-vcs_download_sample_data
 conda config --set anaconda_upload no
