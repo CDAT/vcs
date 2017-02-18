de7c66a73fc492dd402f28c0b3729b608cde489a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-17 16:16:43 -0800

    checking in work so far so i can play with circle-ci under mac

--- tests/vtk/test_vcs_user_passed_date.py
+++
@@ -1,9 +0,0 @@
-import vcs,cdms2,os,sys,cdtime
-import vcs.testing.regression as regression
-
-f=cdms2.open(os.path.join(vcs.sample_data,"clt.nc"))
-s=f("clt",squeeze=1)
-x=regression.init()
-x.plot(s,bg=1,time=cdtime.comptime(2015))
-fnm = os.path.split(__file__)[1][:-3]+".png"
-regression.run(x, fnm)
