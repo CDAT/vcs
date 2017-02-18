0fad5fb07c6845270c0eb0e8097870a0ee1ce1ba
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-17 17:16:01 -0800

    what is downloaded?

--- scripts/circleci_mac_machine_pre.sh
+++ scripts/circleci_mac_machine_pre.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 curl https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -o miniconda.sh
+more miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH=${HOME}/miniconda/bin:${PATH}
 conda config --set always_yes yes --set changeps1 no
