50d0dff1889f0f837cf47aae929e4dc616ef5c5b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-23 16:14:36 -0800

    all mac tests so far

---
+++ tests/test_vcs_taylor_2quads.py
@@ -0,0 +1,17 @@
+import basevcstest
+import MV2
+
+class TestVCSTaylor(basevcstest.VCSBaseTest):
+    def testTaylor2Quads(self):
+
+        data = MV2.array([[-0.50428531,-0.8505522 ,],
+         [ 0.70056821,-0.27235352,],
+         [ 0.05106154, 0.23012322,],
+         [-0.26478429, 0.11950427,],
+         [ 0.85760801,-0.08336641,],
+         [ 1.14083397,-0.78326507,]])
+
+        td = self.x.createtaylordiagram('new')
+        td.quadrans = 2
+        self.x.plot(data, td, skill = td.defaultSkillFunction, bg=1)
+        self.checkImage("test_vcs_taylor_2quads.png")
