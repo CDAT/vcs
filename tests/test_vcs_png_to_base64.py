e8dabf6b749b4596c796e355b3293b573f1d1b6f
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-01 11:05:57 -0800

    png size

---
+++ tests/test_vcs_png_to_base64.py
@@ -0,0 +1,15 @@
+import vcs
+import basevcstest
+
+class TestVCSPNG64(basevcstest.VCSBaseTest):
+    def testPng2Base64(self):
+        m = self.x.createmarker()
+
+        m.type = "star"
+        m.x = [.1]
+        m.y = [.1]
+        m.color = 200
+        m.size = 50
+        display = self.x.plot(m, bg=1)
+
+        display._repr_png_()
