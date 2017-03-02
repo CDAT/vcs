859bd9be7f47c2e33e104e701c7b01e4e91f7697
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-01 16:09:50 -0800

    patterns

---
+++ tests/test_vcs_patterns.py
@@ -0,0 +1,14 @@
+import basevcstest
+import vcs
+
+class TestVCSPatterns(basevcstest.VCSBaseTest):
+    def testPatterns(self):
+        s = self.clt("clt", time=slice(0, 1), squeeze=1)
+        iso = self.x.createisofill("isoleg")
+        iso.levels = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        iso.fillareastyle = "pattern"
+        iso.fillareacolors = vcs.getcolors([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+        iso.fillareaindices = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+        self.x.plot(s, iso, bg=self.bg)
+        fnm = "test_vcs_patterns.png"
+        self.checkImage(fnm)
--- tests/vtk/test_vcs_plot_file_var.py
+++
@@ -1,6 +0,0 @@
-import os, sys, vcs, cdms2
-
-f = cdms2.open(os.path.join(vcs.sample_data,"clt.nc"))
-V = f("clt")
-x = vcs.init()
-x.plot(V, bg=1)
