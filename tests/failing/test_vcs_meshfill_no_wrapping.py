65fec322262305ddf2ff49150d4332153a57ddc5
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-06 23:31:34 -0800

    finished mesh no wrap and moved to failing while waiting

--- tests/failing/test_vcs_meshfill_no_wrapping.py
+++ tests/failing/test_vcs_meshfill_no_wrapping.py
@@ -1,10 +1,11 @@
 import basevcstest
-import numpy
+import cdms2
+import os
 
 class TestVCSMeshfill(basevcstest.VCSBaseTest):
     def testVCSMeshfillNoWrap(self):
 
-        f = cdms2.open()
+        f = cdms2.open(os.path.join(self.basedatadir,"heat.nc"))
         h = f("heat")
         self.x.plot(h, bg=self.bg)
         self.checkImage("test_vcs_meshfill_no_wrapping.png")
