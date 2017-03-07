f39500c269c9b52a78357deb505d5b9b35216b85
Aashish Chaudhary <aashish24@gmail.com>
2017-03-07 17:59:25 -0500

    Ported extension 2 setting to nose

---
+++ tests/test_vcs_bad_ext_2_setting.py
@@ -0,0 +1,10 @@
+import basevcstest
+import vcs
+
+class TestVCSBadExt2Setting(basevcstest.VCSBaseTest):
+    def testBadExt2Setting(self):
+		iso = self.x.createisofill()
+		iso.levels = [[1.e20,1.e20]]
+		iso.ext_2 = "n"
+
+		assert(iso.levels == [[1.e20,1.e20]])
--- tests/vtk/test_bad_ext_2_setting.py
+++
@@ -1,7 +0,0 @@
-import vcs
-x=vcs.init()
-iso=x.createisofill()
-iso.levels=[[1.e20,1.e20]]
-iso.ext_2="n"
-
-assert(iso.levels == [[1.e20,1.e20]])
