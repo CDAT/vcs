e7348dd8021c17a264949a606668ccbd8b6a9b82
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-07 10:27:15 -0800

    more tests

--- tests/vtk/test_vcs_mercator_edge.py
+++
@@ -1,9 +0,0 @@
-import os, sys, cdms2, vcs, vcs.testing.regression as regression
-
-f = cdms2.open(os.path.join(vcs.sample_data, "clt.nc"))
-s = f("clt")
-x = regression.init()
-iso = x.createisofill()
-iso.projection = "mercator"
-x.plot(s(latitude=(-90, 90)), iso, bg=1)
-regression.run(x, "test_vcs_mercator_edge.png")
