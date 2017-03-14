52b8418704024d78ba4d51b83b7d87e5b5a986b3
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-14 08:31:53 -0700

    isofill on their way

--- tests/vtk/test_vcs_isofill_mask_cell_shift.py
+++
@@ -1,8 +0,0 @@
-import os, sys, MV2, cdms2, vcs, vcs.testing.regression as regression
-
-x = regression.init()
-f = cdms2.open(os.path.join(vcs.sample_data,"clt.nc"))
-s = f("clt",slice(0,1),latitude=(30, 70),longitude=(-130, -60))
-s2 = MV2.masked_greater(s, 65.)
-x.plot(s2,"default","isofill",bg=1)
-regression.run(x, "test_vcs_isofill_mask_cell_shift.png")
