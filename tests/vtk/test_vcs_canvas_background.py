31e8453659b572b9a5d125823c2d6581171017df
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-15 15:39:24 -0700

    vcs bg

--- tests/vtk/test_vcs_canvas_background.py
+++
@@ -1,6 +0,0 @@
-import os, sys, cdms2, vcs, vcs.testing.regression as regression
-
-x = regression.init()
-x.backgroundcolor = (255, 255, 255)
-x.open()
-regression.run(x, "test_backgroundcolor_white.png")
