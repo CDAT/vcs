a77af684b32c1cd3d6ccfea781ab537c68b722da
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-15 13:15:41 -0700

    click marker

--- tests/test_vcs_configurator_click_marker.py
+++ tests/test_vcs_configurator_click_marker.py
@@ -1,20 +1,18 @@
 import basevcstest
 
 class TestVSConfigurator(basevcstest.VCSBaseTest):
-    def testConfiguratorClickTest(self):
-
+    def testClickMarker(self):
         self.x.bgX = 800
         self.x.bgY = 606
-        t = self.x.createtext()
-        t.string = "test string"
-        t.x = .1
-        t.y = .1
+        m = self.x.createmarker()
+        m.x = .1,
+        m.y = .1,
 
         # enable the configurator
         self.x.configure()
 
         # plot in the background
-        dp = self.x.plot(t, bg=True)
+        dp = self.x.plot(m, bg=1)
 
         # Grab the initialized configurator
         c = self.x.configurator
@@ -25,12 +23,18 @@ class TestVSConfigurator(basevcstest.VCSBaseTest):
         w, h = self.x.bgX, self.x.bgY
 
         # Retrieve the actor at the specified point
-        actor = c.actor_at_point(.1 * w + 10, .1 * h + 5)
+        actor = c.actor_at_point(.1 * w, .1 * h)
 
-        self.assertIsNotNone(actor, "Couldn't find text actor")
+        self.assertIsNotNone(actor, "Couldn't find marker actor")
 
         display, key = c.display_and_key_for_actor(actor)
 
         self.assertEqual(display, dp, "Found wrong display")
 
-        self.assertFalse(actor!=display.backend[key] and actor not in display.backend[key], "Found wrong key for actor")
+        try:
+            if actor not in display.backend[key][0]:
+                print "Found wrong key for actor"
+                sys.exit(1)
+        except AttributeError:
+            print "Found wrong key for actor"
+            sys.exit(1)
