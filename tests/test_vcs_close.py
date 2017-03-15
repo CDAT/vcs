985b73711a6aac2cb18ee94116ef76534ec11ca4
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-15 14:50:53 -0700

    close

--- tests/test_vcs_close.py
+++ tests/test_vcs_close.py
@@ -1,7 +1,20 @@
 import basevcstest
+import vcs
 
 class TestVCSClose(basevcstest.VCSBaseTest):
-    def testClose(self):
+    def testCloseBg(self):
+        self.x=vcs.init()
         data = self.clt('clt')
-        self.x.plot(data, bg=1)
+        self.assertFalse(self.x.isopened())
+        self.x.plot(data, bg=True)
+        self.assertFalse(self.x.isopened())
         self.x.close()
+        self.assertFalse(self.x.isopened())
+    def testCloseNoBg(self):
+        self.x=vcs.init()
+        data = self.clt('clt')
+        self.assertFalse(self.x.isopened())
+        self.x.plot(data, bg=False)
+        self.assertTrue(self.x.isopened())
+        self.x.close()
+        self.assertFalse(self.x.isopened())
