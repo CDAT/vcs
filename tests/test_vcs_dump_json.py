2838e1ec06995b400fca0a43fc13d50ec9b6a4b0
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-15 10:47:35 -0700

    test dump json

---
+++ tests/test_vcs_dump_json.py
@@ -0,0 +1,41 @@
+import basevcstest
+import filecmp
+import os
+
+class TestVCSJson(basevcstest.VCSBaseTest):
+    def testDumpJson(self):
+        fnm = "test_vcs_dump_json.json"
+        if os.path.exists(fnm):
+            os.remove(fnm)
+
+        b = self.x.createboxfill("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createisofill("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createisoline("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createmeshfill("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.create1d("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createfillarea("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createvector("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createtext("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createline("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createmarker("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createtemplate("vcs_instance")
+        b.script("test_self.x_dump_json","a")
+        b = self.x.createprojection("vcs_instance")
+        b.script(fnm,"a")
+
+        src =os.path.join(self.basedatadir,"vcs",fnm)
+        print "Comparing:",os.path.realpath(fnm),src
+        self.assertTrue(filecmp.cmp(fnm, src))
+        #os.remove(fnm)
+
+
