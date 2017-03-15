39e2261508daefaa0a06bfa9646d7225ee00e838
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-15 15:34:15 -0700

    bg update

--- tests/vtk/test_vcs_canvas_background_update.py
+++
@@ -1,8 +0,0 @@
-import os, sys, cdms2, vcs, vcs.testing.regression as regression
-
-x = regression.init()
-x.backgroundcolor = (255, 255, 255)
-x.open()
-x.backgroundcolor = (255, 255, 0)
-x.update()
-regression.check_result_image(x, "test_backgroundcolor_yellow.png")
