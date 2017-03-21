a6c63c3baac77ea3a916d288ed8fd9d5ab94c63d
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-20 17:21:49 -0700

    trying to use a nightly debug vtk to see if it helps finding the issue

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -1,5 +1,6 @@
 export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
 export VCS_BACKGROUND=0  # circleci seg faults on bg=1
-python run_tests.py -v2 -g -H -p  # -H and -p for collection by artifacts
+#python run_tests.py -v2 -g -H -p  # -H and -p for collection by artifacts
+python run_tests.py --dropbox -v2 -g --no-vtk-ui -V "conda-forge -c uvcdat/label/debug -c uvcdat"
 
