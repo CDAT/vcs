97a7a347ef1d592ed1c76da08b6ca88fbcf2bc3b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-21 14:04:42 -0700

    no need for bg=False

--- tests/test_vcs_drawLinesAndMarkersLegend.py
+++ tests/test_vcs_drawLinesAndMarkersLegend.py
@@ -1,16 +1,12 @@
 import basevcstest
 
 class TestVCSDrawMarkerLeg(basevcstest.VCSBaseTest):
-    def __init__(self,*a,**k):
-        k["bg"]=False
-        super(TestVCSDrawMarkerLeg,self).__init__(*a,**k)
-
     def testDrawMarkerLegend(self):
         t = self.x.createtemplate()
         t.drawLinesAndMarkersLegend(self.x,
               ["red","blue","green"], ["solid","dash","dot"],[1,4,8],
               ["blue","green","red"], ["cross","square","dot"],[3,4,5],
-              ["sample A","type B","thing C"],bg=self.bg,render=True)
+              ["sample A","type B","thing C"],render=True,bg=self.bg)
 
         fnm = "test_drawLinesAndMarkersLegend.png"
         self.checkImage(fnm)
