18ac518bdc037e707ae7b85668863950e9ca8c30
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-11 09:45:44 -0700

    regional lambert azimuthal

--- tests/test_vcs_lambert_azimuthal.py
+++ tests/test_vcs_lambert_azimuthal.py
@@ -2,20 +2,23 @@ import basevcstest
 
 
 class TestVCSLambertAzimuthal(basevcstest.VCSBaseTest):
-    def main(self, centerlongitude):
-        s = self.clt("clt", slice(0, 1))
+    def main(self, centerlongitude, centerlatitude):
+        s = self.clt("clt", time=slice(0, 1),
+                longitude=(centerlongitude - 40, centerlongitude + 30),
+                latitude=(centerlatitude-20,centerlatitude+30))
         gm = self.x.createboxfill()
         p = self.x.createprojection()
         p.type = "lambert azimuthal"
         p.centerlongitude = centerlongitude
+        p.centerlatitude = centerlatitude
         gm.projection = p
-        self.x.plot(s(longitude=(centerlongitude - 30, centerlongitude + 30)), gm, bg=self.bg)
+        self.x.plot(s, gm, bg=self.bg)
         fileName = "test_vcs_lambert_azimuthal_" + str(centerlongitude) +\
                    ".png"
         self.checkImage(fileName)
 
     def testVCSLambertAzimuthal(self):
         # north america
-        self.main(centerlongitude=-97)
+        self.main(centerlongitude=-97, centerlatitude=35)
         # africa
-        self.main(centerlongitude=21)
+        self.main(centerlongitude=21, centerlatitude=-5)
