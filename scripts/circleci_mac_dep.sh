0e03c743d78ee466beac29cf5c66e38f2f98092b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-18 11:53:47 -0700

    trying to see if mesa mac owrks better

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -4,5 +4,6 @@ pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
 conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat uvcdat pyopenssl nose image-compare flake8 "mesalib<8"
 pip install dropbox
+conda install --force -c uvcdat/label/testmesa -c conda-forge -c uvcdat vtk-cdat-nox
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install --old-and-unmanageable
