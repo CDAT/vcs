61dfa6738cf74f5c7c34013077a06a0755829342
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-19 08:14:47 -0700

    updated .travis and circleci to get only what's needed

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat uvcdat pyopenssl nose image-compare flake8 "mesalib<8"
+conda install -c uvcdat/label/testmesa -c uvcdat/label/nightly -c conda-forge -c uvcdat cdutil genutil dv3d-nox "mesalib>17" nose image-compare flake8
 pip install dropbox
 conda install --force -c uvcdat/label/testmesa -c conda-forge -c uvcdat vtk-cdat-nox mesalib>8
 export UVCDAT_ANONYMOUS_LOG=False
