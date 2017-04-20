a905f85a43d90930c5ec65a7da24a9486c2fda47
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-20 09:28:30 -0700

    testing mesa

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -4,6 +4,6 @@ pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
 conda install -c uvcdat/label/testmesa -c uvcdat/label/nightly -c conda-forge -c uvcdat cdutil genutil dv3d-nox "mesalib>17" nose image-compare flake8
 pip install dropbox
-conda install --force -c uvcdat/label/testmesa -c conda-forge -c uvcdat vtk-cdat-nox mesalib>8
+#conda install --force -c uvcdat/label/testmesa -c conda-forge -c uvcdat vtk-cdat-nox mesalib>17
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install --old-and-unmanageable
