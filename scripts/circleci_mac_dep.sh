2f18c4bddef2e97c410b1f81e95fd321e577076b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-24 12:57:00 -0700

    adding matplotlib

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,8 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/testmesa -c uvcdat/label/nightly -c conda-forge -c uvcdat cdutil genutil dv3d-nox "mesalib>17" nose image-compare flake8
+conda install -c uvcdat/label/testmesa -c uvcdat/label/nightly -c conda-forge -c uvcdat cdutil genutil dv3d-nox mesalib nose image-compare flake8 matplotlib
 pip install dropbox
-#conda install --force -c uvcdat/label/testmesa -c conda-forge -c uvcdat vtk-cdat-nox mesalib>17
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install --old-and-unmanageable
