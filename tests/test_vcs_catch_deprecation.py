959bf2a3ea5289328c2394c5a6031732556dfdd9
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-02 09:24:07 -0700

    cleaned up test prints

--- tests/test_vcs_catch_deprecation.py
+++ tests/test_vcs_catch_deprecation.py
@@ -8,7 +8,4 @@ class VCSDeprecationTest(unittest.TestCase):
     def testCatchDeprecationWarning(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.warn("Can you catch me?",vcs.VCSDeprecationWarning)
-            #warnings.warn("Can you catch me?",DeprecationWarning)
             self.assertEqual(w[0].category, vcs.VCSDeprecationWarning)
-            print w
-            print w[0].category
