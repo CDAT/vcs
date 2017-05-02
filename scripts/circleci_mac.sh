3bbe6892ddeb3ab47b936e2dc722b5f88e29fb5a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-02 14:57:29 -0700

    mac upload from cicrcleci

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -3,4 +3,6 @@ export PATH=${HOME}/miniconda/bin:${PATH}
 #export VCS_BACKGROUND=0  # circleci seg faults on bg=1
 #python run_tests.py -v2 -g -H -p  # -H and -p for collection by artifacts
 python run_tests.py --dropbox -v2 -g --no-vtk-ui
+if [ ${rc} -ne 0 ]; then exit ${rc} ; fi
+if [ ${rc} -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
 
