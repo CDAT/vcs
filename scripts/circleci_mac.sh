484f5835eaf3aff99e8ffc5f9386700cde37ca2d
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-26 09:58:44 -0700

    no more dropbox

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -2,7 +2,7 @@ export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
 #export VCS_BACKGROUND=0  # circleci seg faults on bg=1
 #python run_tests.py -v2 -g -H -p  # -H and -p for collection by artifacts
-python run_tests.py --dropbox -v2 -g --no-vtk-ui
+python run_tests.py -v2 -g --no-vtk-ui
 RESULT=$?
 echo "test command exit result:",$RESULT
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./scripts/conda_upload.sh ; fi
