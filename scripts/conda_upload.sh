c4fd9cc3812fa5b6c923457ee12d162726a7207b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-06-26 16:16:53 -0700

    test for slowness, and better way to set layer (#200)
    * test for slowness, and better way to set layer
    
    * trst very sensitive to sys load so increased threshold a bit
    
    fixed typo
    added streamline to vcs.creategrpahicsmethod function
    
    * fixed upload script
    
    * better way to see if constant increase in time
    
    * flake8
    
    * scaling elapsed times
    
    * tests seem to pass on my mac now
    
    * no more dropbox
    
    * adding version to pkg name upload
    
    * seems like travis craps on itself if too many at once

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-PKG_NAME=cdms2
+PKG_NAME=vcs
 USER=uvcdat
+VERSION="2.10"
 echo "Trying to upload conda"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
@@ -15,17 +16,11 @@ fi
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
-export VERSION=`date +%Y.%m.%d`
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
 python ./prep_for_build.py
-conda build vcs -c conda-forge -c uvcdat 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y*`-py27_0.tar.bz2 --force
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    python ./prep_for_build.py -v `date +%Y.%m.%d` -f nox
-    conda build vcs -c conda-forge -c uvcdat 
-    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-nox-`date +%Y*`-py27_0.tar.bz2 --force
-fi
+conda build $PKG_NAME -c conda-forge -c uvcdat 
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION.`date +%Y*`-0.tar.bz2 --force
