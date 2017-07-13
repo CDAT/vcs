998d06728d4c54f658974a4de96180dd928f0bb1
Charles Doutriaux <doutriaux1@llnl.gov>
2017-07-12 17:17:25 -0700

    making sure we get latest mesalib

--- scripts/circleci_mac_dep.sh
+++ scripts/circleci_mac_dep.sh
@@ -2,7 +2,7 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat vtk-cdat cdutil genutil dv3d mesalib nose image-compare flake8 matplotlib
+conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat vtk-cdat cdutil genutil dv3d "mesalib=17.4.1.=3" nose image-compare flake8 matplotlib
 pip install dropbox
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install --old-and-unmanageable
