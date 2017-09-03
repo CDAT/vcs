dcb645ebf05253a0daba14f18d9fc23bc8bdfd59
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-02 18:12:17 -0700

    1 for circleci ?

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -1,7 +1,7 @@
 export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
 #export VCS_BACKGROUND=0  # circleci seg faults on bg=1
-python run_tests.py -n 2 -v2 -g --no-vtk-ui
+python run_tests.py -n 1 -v2 -g --no-vtk-ui
 RESULT=$?
 echo "test command exit result:",$RESULT
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then conda install conda-build anaconda-client ; fi
