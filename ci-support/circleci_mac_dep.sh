d5a682e3de165d67d2bbe44c9b3e16e9133e4efe
Charles Doutriaux <doutriaux1@llnl.gov>
2017-10-04 09:37:00 -0700

    looks lie matplotlib wants a big six

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,6 +2,6 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat vtk-cdat cdutil genutil dv3d mesalib nose image-compare flake8 matplotlib
+conda install -c uvcdat/label/nightly -c conda-forge -c uvcdat vtk-cdat cdutil genutil dv3d mesalib nose image-compare flake8 matplotlib "six>=1.10"
 export UVCDAT_ANONYMOUS_LOG=False
 python setup.py install --old-and-unmanageable
