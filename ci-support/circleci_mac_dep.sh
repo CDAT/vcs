77a8a40533dce035da967ec2f530d4a94d87bc41
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-16 06:39:44 -0800

    mac needs vtk-cdat

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil netcdf-fortran=4.4.4=3
-conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil netcdf-fortran=4.4.4=3
+conda create -q -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil netcdf-fortran=4.4.4=3 vtk-cdat
+conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil netcdf-fortran=4.4.4=3 vtk-cdat
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
 mkdir gits
