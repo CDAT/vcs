80de2c411dd1b3b8affeef7da043b1063d8bf799
Jonathas Costa <jon.costa@gmail.com>
2018-01-22 11:49:40 -0500

    Improved DejaVu font test.

--- tests/test_vcs_dejavu_fonts.py
+++ tests/test_vcs_dejavu_fonts.py
@@ -4,5 +4,4 @@ import basevcstest
 class TestVCSTextsExtents(basevcstest.VCSBaseTest):
     def testDejaVuFonts(self):
 
-        self.x.switchfonts("default", "DejaVuSans")
-        assert(self.x.getfontname(1) == "DejaVuSans")
+        assert(self.x.getfont("DejaVuSans") == 21)
