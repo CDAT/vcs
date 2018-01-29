0c0c2be1beb965c4a133563e9d7ee17ff66fc991
Charles Doutriaux <doutriaux1@llnl.gov>
2018-01-29 07:01:55 -0800

    added deps for make html

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py3 -c uvcdat/label/nightly -c nesii/channel/dev-esmf  -c conda-forge -c uvcdat "cdms2>2.12.2017" nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat "dv3d>2.12.2017" "cdutil>2.12.2017" "cdtime<2018"
-conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat "cdms2>2.12.2017" cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat "dv3d>2.12.2017" "cdutil>2.12.2017" "cdtime<2018"
+conda create -q -n py3 -c uvcdat/label/nightly -c nesii/channel/dev-esmf  -c conda-forge -c uvcdat "cdms2>2.12.2017" nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat "dv3d>2.12.2017" "cdutil>2.12.2017" "cdtime<2018" nbsphinx easydev
+conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat "cdms2>2.12.2017" cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat "dv3d>2.12.2017" "cdutil>2.12.2017" "cdtime<2018" nbsphinx easydev
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
 python setup.py install --old-and-unmanageable
