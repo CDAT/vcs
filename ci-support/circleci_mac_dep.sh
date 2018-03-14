8a1b27a36cc12c8084994c8de81ad2f4776a5dc4
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-13 18:44:38 -0700

    Circleci no mpl restr, no vtkcdat

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py3 -c cdat/label/nightly -c uvcdat/label/nightly -c nesii/channel/dev-esmf  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat dv3d cdutil cdtime
-conda create -q -n py2 -c cdat/label/nightly -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat dv3d cdutil cdtime nbsphinx easydev
+conda create -q -n py3 -c cdat/label/nightly -c uvcdat/label/nightly -c nesii/channel/dev-esmf  -c conda-forge -c uvcdat cdms2 nose flake8 "python>3" cdat_info udunits2 mesalib nose image-compare flake8 matplotlib numpy=1.13 image-compare genutil dv3d cdutil cdtime
+conda create -q -n py2 -c cdat/label/nightly -c uvcdat/label/nightly -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 matplotlib numpy=1.13 image-compare genutil dv3d cdutil cdtime nbsphinx easydev
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
 python setup.py install --old-and-unmanageable
