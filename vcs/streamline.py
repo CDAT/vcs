3c2568bcb9433277271c604aa94ab495fba11289
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-19 12:50:55 -0600

    Work around axis convert for these plots until fixed

--- vcs/streamline.py
+++ vcs/streamline.py
@@ -247,6 +247,8 @@ class Gs(vcs.bestMatch):
             self,
             'xaxisconvert',
             value)
+        if value != 'linear':
+            raise ValueError('streamline currently cannot handle axis scaling')
         self._xaxisconvert = value
     xaxisconvert = property(_getxaxisconvert, _setxaxisconvert)
 
@@ -258,6 +260,8 @@ class Gs(vcs.bestMatch):
             self,
             'yaxisconvert',
             value)
+        if value != 'linear':
+            raise ValueError('streamline currently cannot handle axis scaling')
         self._yaxisconvert = value
     yaxisconvert = property(_getyaxisconvert, _setyaxisconvert)
 
