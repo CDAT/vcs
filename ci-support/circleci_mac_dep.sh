ba1cbfe2212b726711192796314b8957123d8f77
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-23 10:31:15 -0700

    need matplotlib

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py2 -c nesii/label/dev-esmf  -c conda-forge -c cdat/label/v80 vcs mesalib image-compare nose flake8 "python<3"
-conda create -q -n py3 -c nesii/label/dev-esmf  -c conda-forge -c cdat/label/v80 vcs mesalib image-compare nose flake8 "python>3"
+conda create -q -n py2 -c nesii/label/dev-esmf  -c conda-forge -c cdat/label/v80 vcs matplotlib mesalib image-compare nose flake8 "python<3"
+conda create -q -n py3 -c nesii/label/dev-esmf  -c conda-forge -c cdat/label/v80 vcs matplotlib mesalib image-compare nose flake8 "python>3"
 export UVCDAT_ANONYMOUS_LOG=False
 git clone git://github.com/uv-cdat/uvcdat-testdata
 cd uvcdat-testdata
