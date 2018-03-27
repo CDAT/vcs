47c06c570fa05a53a292f1bef8534b69ba484b70
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-27 16:46:45 -0700

    cleanup build command

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,8 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 matplotlib numpy=1.13 image-compare genutil dv3d cdutil cdtime "proj4<5" "python<3" "numpy>1.14" 
-conda create -q -n py3 -c cdat/label/nightly -c nesii/label/dev-esmf  -c conda-forge -c uvcdat cdms2 nose flake8 cdat_info udunits2 mesalib nose image-compare flake8 matplotlib numpy=1.13 image-compare genutil dv3d cdutil cdtime nbsphinx easydev "proj4<5" "python>3" "numpy>1.14" 
+conda create -q -n py2 -c cdat/label/nightly -c conda-forge -c cdat cdms2 cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 matplotlib image-compare genutil dv3d cdutil cdtime "proj4<5" "python<3" "numpy>1.14" 
+conda create -q -n py3 -c cdat/label/nightly -c conda-forge -c cdat cdms2 nose flake8 cdat_info udunits2 mesalib nose image-compare flake8 matplotlib image-compare genutil dv3d cdutil cdtime nbsphinx easydev "proj4<5" "python>3" "numpy>1.14" 
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
 python setup.py install --old-and-unmanageable
--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -2,7 +2,6 @@
 PKG_NAME=vcs
 USER=cdat
 VERSION="2.12"
-ESMF_CHANNEL="nesii/label/dev-esmf"
 echo "Trying to upload conda"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
@@ -17,11 +16,11 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=${HOME}/conda-bld
 echo "Cloning recipes"
-git clone git://github.com/UV-CDAT/conda-recipes
+git clone git://github.com/CDAT/conda-recipes
 cd conda-recipes
 # uvcdat creates issues for build -c uvcdat confises package and channel
 rm -rf uvcdat
 python ./prep_for_build.py
-conda build $PKG_NAME -c conda-forge -c cdat -c uvcdat 
-conda build $PKG_NAME -c ${ESMF_CHANNEL} -c uvcdat/label/nightly -c conda-forge -c cdat -c uvcdat --python=3.6
+conda build $PKG_NAME -c conda-forge -c cdat -c uvcdat --python=27
+conda build $PKG_NAME -c cdat/label/nightly -c conda-forge -c cdat -c uvcdat --python=3.6
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION.`date +%Y*`0.tar.bz2 --force
