eefb5e8546475d4adef40d7e8bc07ce7b5577674
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-28 18:05:41 -0600

    Update marker sizes in test to match baseline

--- tests/test_vcs_drawLinesAndMarkersLegend.py
+++ tests/test_vcs_drawLinesAndMarkersLegend.py
@@ -8,7 +8,7 @@ class TestVCSDrawMarkerLeg(basevcstest.VCSBaseTest):
                                     ["red", "blue", "green"], [
                                         "solid", "dash", "dot"], [1, 4, 8],
                                     ["blue", "green", "red"], [
-                                        "cross", "square", "dot"], [.2, .6, 5],
+                                        "cross", "square", "dot"], [10, 10, 10],
                                     ["sample A", "type B", "thing C"], render=True, bg=self.bg)
 
         fnm = "test_drawLinesAndMarkersLegend.png"
@@ -21,11 +21,9 @@ class TestVCSDrawMarkerLeg(basevcstest.VCSBaseTest):
         t.legend.y2 = .65
         ids = ["Sea Level Pressure (ERA-Interim)","SW Cloud Forcing (CERES-EBAF 4.0)","LW Cloud Forcing (CERES-EBAF 4.0)","Land Precipitation (GPCP 2.2)","Ocean Precipitation (GPCP 2.2)","2-m temperature","More"]
         #ids = ["SLP(ERA-Interim)","A2","A3","B1","C1","C2","C3"]
-        id_sizes = [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5,]
+        id_sizes = [18, 18, 18, 18, 18, 18, 18,]
         id_colors = ["red","orange","green","cyan","blue","purple","black"]
         symbols = ["diamond","square_fill","circle","triangle_right_fill","triangle_left_fill","triangle_up_fill","triangle_down_fill"]
-        colors = ["red","orange","green","cyan","blue","purple","black"]
-        sizes = [20., 20., 20., 20., 20., 20., 20.,]
         n = 7
         t.drawLinesAndMarkersLegend(self.x, linecolors=[[0,0,0,0]]*n, linetypes=['solid',]*n, linewidths=[0,]*n, markercolors=id_colors, markertypes=symbols, markersizes=id_sizes, strings=ids, scratched=None, stringscolors=id_colors, stacking='vertical', bg=False, render=True)
 
