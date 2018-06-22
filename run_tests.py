e449e232c73272237527523221bdb990e37fb408
Charles Doutriaux <doutriaux1@llnl.gov>
2018-06-22 13:23:58 -0700

    linux paths are case sensitive

--- run_tests.py
+++ run_tests.py
@@ -11,14 +11,16 @@ class VCSTestRunner(cdat_info.TestRunnerBase):
             opt += ["-A",  "not vtk_ui"]
         if self.args.vtk is not None:
             vtk_name = "vtk-cdat"
-            cdat_info.run_command("conda install -f -y -c %s %s" % (self.args.vtk, vtk_name))
+            cdat_info.run_command(
+                "conda install -f -y -c %s %s" % (self.args.vtk, vtk_name))
         return opt
 
 
 test_suite_name = 'vcs'
 
 workdir = os.getcwd()
-runner = VCSTestRunner(test_suite_name, options=["--no-vtk-ui", "--vtk"], options_files=["tests/vcs_runtests.json"], get_sample_data=True)
+runner = VCSTestRunner(test_suite_name, options=[
+                       "--no-vtk-ui", "--vtk"], options_files=["tests/vcs_runtests.json"], get_sample_data=True)
 ret_code = runner.run(workdir)
 
 sys.exit(ret_code)
