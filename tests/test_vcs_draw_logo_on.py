dadf5d017feaa2116e3d8599cb3698f51a2d1af1
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-12-19 12:17:04 -0700

    Fix aspect ratio of logo image so it is not stretched or squashed (#376)
    Also add two new tests that make sure logo stays the right aspect
    ratio whether the canvas is tall/narrow or short/wide.

--- tests/test_vcs_draw_logo_on.py
+++ tests/test_vcs_draw_logo_on.py
@@ -14,3 +14,17 @@ class TestVCSDrawLogo(basevcstest.VCSBaseTest):
         self.x.plot(a, bg=self.bg)
         fnm = "test_vcs_draw_logo_on.png"
         self.checkImage(fnm)
+
+        self.x.clear()
+        self.x.drawlogoon()
+        self.x.geometry(800, 400)
+        self.x.plot(a, bg=self.bg)
+        fnm = "test_vcs_draw_logo_on_wide.png"
+        self.checkImage(fnm)
+
+        self.x.clear()
+        self.x.drawlogoon()
+        self.x.geometry(400, 800)
+        self.x.plot(a, bg=self.bg)
+        fnm = "test_vcs_draw_logo_on_tall.png"
+        self.checkImage(fnm)
